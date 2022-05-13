11103facfdf79bc248132818e836ad1a2b07fdd6
Eric Chen <eschen@cpp.edu>
2022-05-12 21:47:09 -0700

    added mm and time testing

--- sortAlgo.java
+++ sortAlgo.java
@@ -2,24 +2,29 @@
  * Eric Chen
  * CS3310 Project 2
  */
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class sortAlgo {
 
     /**
      * Merge Sort O(nlogn)
      * @param   
      */
-    static int pivotposition = 0;
+    //static int pivotposition = 0;
 
      
-    public static int[] mergeSort(int[] arr,int low, int high){
+    public static int mergeSort(int[] arr,int low, int high, int k){
         int mid = 0;
         if(low < high){
             mid = ((low + high) / 2);
-            mergeSort(arr,low, mid);
-            mergeSort(arr,mid+1, high);
+            mergeSort(arr,low, mid, k);
+            mergeSort(arr,mid+1, high, k);
             merge(arr, low, mid, high);
         }
-        return arr;
+        return arr[k];
     }
 
     public static void merge(int[]arr,int low, int mid, int high){
@@ -58,16 +63,44 @@ public class sortAlgo {
     }
 
     /**
+     * Iterative Quick Sort
+     * @param  n  number of elements
+     * @param  k key
+     * @return the kth smallest number
+     */
+    public static int IterQuickSort(int[] arr, int n, int k){
+        int m = 0;
+        int j = n;
+        int pivot = arr.length - 1;
+        for(int i = 0; i <= n ; i++ ){
+            Partition(arr, m, j, pivot);
+            if(k == Partition(arr, m, j, pivot)){
+                return arr[k];
+            }
+
+            if(k < Partition(arr, m, j, pivot)){
+                j = Partition(arr, m, j, pivot) - 1;
+            } else{
+                m = Partition(arr, m, j, pivot) + 1;
+                //k = k - pivot;
+            }
+            
+        }
+
+        return arr[k];
+    }
+
+    /**
      * Recursive Quick Sort
      * @param   
      */
-    public static int[] RecurQuickSort(int[] arr,int p, int q){
+    public static int RecurQuickSort(int[] arr,int p, int q, int pivotposition){
         if(p < q){
             Partition(arr, p,q, pivotposition);
-            RecurQuickSort(arr, p,Partition(arr, p,q, pivotposition) - 1 );
-            RecurQuickSort(arr,Partition(arr, p,q, pivotposition) + 1,q);
+            RecurQuickSort(arr, p,Partition(arr, p,q, pivotposition) - 1, pivotposition );
+            RecurQuickSort(arr,Partition(arr, p,q, pivotposition) + 1,q,pivotposition);
         }
-        return arr;
+        return arr[pivotposition];
     }  
 
 
@@ -95,40 +128,57 @@ public class sortAlgo {
         return pivot;
     }
 
-    /**
-     * Iterative Quick Sort
-     * @param  n  number of elements
-     * @param  k key
-     * @return the smallest number
-     */
-    public static int IterQuickSort(int[] arr, int n, int k){
-        int m = 0;
-        int j = n;
-        int pivot = arr.length - 1;
-        for(int i = 0; i <= n ; i++ ){
-            Partition(arr, m, j, pivot);
-            if(k == Partition(arr, m, j, pivot)){
-                return arr[k];
+
+    public static int getMedian(int arr[], int i, int n){
+        Arrays.sort(arr, i, n);
+        return arr[i+(n-i)/2];  
+    }
+
+    public static int mmSort(int arr[], int low, int high, int k){
+        int n = high - low + 1;
+        int r = 5;
+
+        if (k > 0 && k <= n){
+            int i;
+
+            int[] median = new int[(n + 4)/r];
+            for(i = 0; i < n/5; i++){
+                median[i] = getMedian(arr, low + (i*r), low + (i*r) + 5);
             }
 
-            if(k < Partition(arr, m, j, pivot)){
-                j = Partition(arr, m, j, pivot) - 1;
-            } else{
-                m = Partition(arr, m, j, pivot) + 1;
-                //k = k - pivot;
+            if(i * 5 < n){
+                median[i] = getMedian(arr, low + (i*r), low + (i*5) + (n%5));
+                i++;
             }
-            
+
+            int v = mmSort(median, 0, i - 1, i /2);
+
+            int pos = Partition(arr, low, high, v);
+
+            if(pos - low == k - 1){
+                return arr[pos];
+            }
+            if(pos - low >(k - 1)){
+                return mmSort(arr, low, pos - 1, k);
+            } else{
+                return mmSort(arr, pos + 1, high, k - pos + low - 1);
+            }
         }
+
+
 
         return arr[k];
     }
 
-    /**
-     * Median of Medians
-     * @param   
-     */
-    public static double mmSort(){
-        return 0;
+    //Method to randomly generate test matrices. Range of numbers from -100 to 100.
+    public static int[] fillArr(int n, int[] arr){
+        Random rand = new Random();
+        int randInt = 0;
+        for(int i = 0; i < n; i++){
+                randInt = -100 + rand.nextInt(200); // Random Int from -100-100
+                arr[i] = randInt;
+        }
+        return arr;
     }
 
     public static void printArr(int[] arr){
@@ -138,14 +188,74 @@ public class sortAlgo {
         }
     }
     public static void main(String[] args) {
-        int[] testArr = {16, 4, 5,23};
-        int[] testArr2 = {13,27,5,26,547,43,2,46,23};
-        //System.out.println("From the main method:");
-        //printArr(mergeSort(testArr, 0, 6));
-        //printArr(RecurQuickSort(testArr, 0, testArr.length));
-        for(int i = 0; i < testArr2.length; i++)
-        System.out.println("The lowest number in iterQuick is: "+IterQuickSort(testArr2, testArr2.length, i));
-    }
 
-    
+        //Taking user input for array size and kth value
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Please enter the number of elements in the array. (10,50,100,500,1000): ");
+        int arrSize = sc.nextInt();
+        System.out.printf("Enter the kth element(1 being the least): ");
+        int k = sc.nextInt();
+        sc.close();
+
+        int iter = 100; //Set number iterations
+        int[] arr = new int[arrSize];
+        arr = fillArr(arrSize, arr); //Randomize input array
+        System.out.println("The numbers in the array are: ");
+        printArr(arr);
+
+        
+        long mergeStart;
+        long mergeEnd;
+        long iterStart;
+        long iterEnd;
+        long recurStart;
+        long recurEnd;
+        long mmStart;
+        long mmEnd;
+
+        long totalTimeMerge = 0;
+        long totalTimeIter = 0;
+        long totalTimeRecur =0;
+        long totalTimeMM = 0;
+
+        if(k >= 1 && arrSize > 0){
+            for(int i = 0; i < iter; i++){
+                mergeStart = System.nanoTime();
+                mergeSort(arr, 0, arrSize - 1, k - 1);
+                mergeEnd = System.nanoTime();
+
+                iterStart = System.nanoTime();
+                IterQuickSort(arr, arrSize, k - 1);
+                iterEnd = System.nanoTime();
+
+                recurStart = System.nanoTime();
+                RecurQuickSort(arr, 0, arrSize - 1, k - 1);
+                recurEnd = System.nanoTime();
+
+                mmStart = System.nanoTime();
+                mmSort(arr,0, arrSize - 1, k);
+                mmEnd = System.nanoTime();
+
+                totalTimeMerge += (mergeEnd - mergeStart);
+                totalTimeRecur += (recurEnd - recurStart);
+                totalTimeIter += (iterEnd - iterStart);
+                totalTimeMM += (mmEnd - mmStart);
+            }
+        } else{
+            System.out.println("Kth value and array size must be set to 1 or higher.");
+            return;
+        }
+
+        System.out.printf("\nThe average time to find the kth smallest element using merge sort is: %f nanoseconds" , (float)(totalTimeMerge / iter));
+        System.out.printf("\nThe average time to find the kth smallest element using iterative quick sort is: %f nanoseconds" , (float)(totalTimeIter/ iter));
+        System.out.printf("\nThe average time to find the kth smallest element using recursive quick sort is: %f nanoseconds" , (float)(totalTimeRecur/ iter));
+        System.out.printf("\nThe average time to find the kth smallest element using Median of Medians is: %f nanoseconds" , (float)(totalTimeMM/ iter));
+
+        System.out.println("\n\nKth element for merge sort: "+mergeSort(arr, 0, arrSize - 1, k-1));
+        System.out.println("Kth element for recursive quick sort: "+RecurQuickSort(arr, 0, arrSize - 1, k-1));
+        System.out.println("Kth element for iterative quick sort: "+IterQuickSort(arr, arrSize, k-1));
+        System.out.println("Kth element for Median of Medians: "+mmSort(arr,0, arrSize - 1, k));
+
+
+    }
 }
